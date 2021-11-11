aae3293d27ea51d586be7c35e71ad44eb05f3c9b
= <salaslilia00@gmail.com>
2021-11-10 22:04:36 -0500

    colors back to normal

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,32 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FaFacebookSquare } from 'react-icons/fa';
 import { FaInstagramSquare } from 'react-icons/fa';
 import { FaTwitterSquare } from 'react-icons/fa';
+import emailjs from 'emailjs-com';
 
 export default function Contact(props) {
+	const form = useRef();
+
+	const sendEmail = e => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				'service_0q5n6uk',
+				'template_hcn0a9h',
+				form.current,
+				'user_IUbfjy62Q6MDwAoXavPIX'
+			)
+			.then(
+				result => {
+					console.log(result.text);
+				},
+				error => {
+					console.log(error.text);
+				}
+			);
+	};
+
 	return (
-		<div className="ContactPage">
-			<div className="form">
-				<h1 className="l-heading">
-					<span className="text-primary">Contact</span> Us
-				</h1>
-				<p>Please fill out the form below to contact us</p>
-				<form>
-					<div>
-						<label>Name</label>
-						<input type="text" name="name" id="name" />
-					</div>
-					<div>
-						<label>Email</label>
-						<input type="email" name="email" id="email" />
-					</div>
-					<div>
-						<label>Message</label>
-						<textarea type="email" name="email" id="email"></textarea>
-					</div>
-					<button className="btn">Send</button>
-				</form>
-			</div>
+		<div>
+			<form ref={form} onSubmit={sendEmail}>
+				<label class="form-control">Name</label>
+				<input class="form-control" type="text" name="user_name" />
+				<label class="form-control">Email</label>
+				<input class="form-control" type="email" name="user_email" />
+				<label class="form-control">Message</label>
+				<textarea class="form-control" name="message" />
+				<input type="submit" value="Send" />
+			</form>
 		</div>
 	);
 }
