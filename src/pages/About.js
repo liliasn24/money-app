84e0a4041b0f874abb267d9ccd2ae2b96d1fd1ad
= <salaslilia00@gmail.com>
2021-11-16 22:27:31 -0500

    about page coming along

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,9 @@ import Main from '../pages/Main';
 export default function About(props) {
 	return (
 		<div className="AboutPage">
-			<div>
-				<h1>Sendalo</h1>
-				<p>
+			<div class="jumbotron text-center">
+				<h1 class="display-4 mb-3">Sendalo</h1>
+				<p class="text-justify mx-5">
 					Sendalo is a mock of a money remittance app intended for people who
 					have no access to traditional financial services in the US. It lets
 					you see how much money you would have for retirement if you save a
